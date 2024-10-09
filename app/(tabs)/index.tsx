eae673642cb5581f06b1ccd40ead087da29c7fd5
Yash Sharma <yashkr00129@gmail.com>
2024-10-09 19:05:06 +0530

    Make Articles  Screen and setup tabs

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import Article from "@/components/Article";
+import { Text, View } from "react-native";
+
+export default function Index() {
+	return (
+		<View
+			style={{
+				flex: 1,
+			}}
+		>
+			<Text>Home</Text>
+		</View>
+	);
+}
---
+++ app/(tabs)/prefrences.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function prefrences() {
+	return (
+		<View>
+			<Text>prefrences</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({});
